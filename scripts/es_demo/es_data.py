71259c8af239cd0cb5b9e1828850c93b37424f18
Farhabi Helal <farhabi.helal@jp.honda-ri.com>
2022-12-16 17:12:00 +0900

    added es data.

---
+++ scripts/es_demo/es_data.py
@@ -0,0 +1,49 @@
+session_data = {
+    "1": {
+        "1": [
+            "topic-intro",
+            "topic-day-one-session-one-names-origins",
+            "topic-day-one-session-one-transition-age",
+            "topic-day-one-session-one-age",
+        ],
+        "2": [
+            "topic-day-one-session-two-intro",
+            "topic-travel-homecountry",
+            "topic-day-one-session-two-transition",
+            "topic-hometown",
+            "topic-day-one-session-two-outro",
+        ],
+    },
+    "2": {
+        "1": [
+            "topic-day-two-session-one-intro",
+            "topic-day-two-family",
+            "topic-day-two-session-one-transition",
+            "topic-day-two-parents",
+            "topic-day-two-session-one-outro",
+        ],
+        "2": [
+            "topic-day-two-session-two-intro",
+            "topic-pet-new",
+            "topic-day-two-session-two-transition",
+            "topic-lemurs",
+            "topic-day-two-session-two-end",
+        ],
+    },
+    "3": {
+        "1": {
+            "topic-day-three-session-one-intro",
+            "topic-day-three-food",
+            "topic-day-three-session-one-transition",
+            "topic-birthday",
+            "topic-day-three-session-one-outro",
+        },
+        "2": {
+            "topic-day-three-session-two-intro",
+            "topic-sports",
+            "topic-day-three-session-two-transition",
+            "topic-day-three-hobbies",
+            "topic-day-three-session-two-outro",
+        },
+    },
+}
