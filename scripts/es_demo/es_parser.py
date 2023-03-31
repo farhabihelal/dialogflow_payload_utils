79cdadb530b81796cfbe25b835f1d8e4ae99221e
Farhabi Helal <farhabi.helal@jp.honda-ri.com>
2023-03-31 14:02:01 +0900

    ignoring unrelated sheets in ES parser.

--- scripts/es_demo/es_parser.py
+++ scripts/es_demo/es_parser.py
@@ -8,15 +8,41 @@ sys.path.append(os.path.abspath(f"{os.path.dirname(__file__)}/../../src"))
 
 from dialogflow_payload_gen.parser_xl import ParserXL
 
+import re
+import pandas as pd
+
 
 class ESParser(ParserXL):
     def __init__(self, config) -> None:
-
         super().__init__(config)
+
+    def load(self, filepath=None):
+        filepath = filepath if filepath else self._config["filepath"]
+
+        self._data = []
+        self._xl_sheets = {}
+
+        self._xl = pd.ExcelFile(filepath)
+
+        for sheet in self._xl.sheet_names:
+            if not re.match("day\-\d\-session\-\d", sheet):
+                continue
+
+            df = pd.read_excel(self._xl, sheet_name=sheet)
+            self._xl_sheets[sheet] = df
+            df.fillna("", inplace=True)
+
+            self._headers[sheet] = df.columns.values.tolist()
+            self._data.extend(df.values.tolist())
+            self._data_sheets[sheet] = df.values.tolist()
+
+        self._header = next(iter(self._headers.values()))
+        self._header_map = {header: i for i, header in enumerate(self._header)}
+        self.unique_intents = self.get_unique_intents()
+        self.intent_names = list(self.unique_intents.keys())
 
 
 if __name__ == "__main__":
-
     title = "es parser"
     version = "0.1.0"
     author = "Farhabi Helal"
